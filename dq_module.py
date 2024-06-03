7ee71f5a986aa4e637a24d9163b6aa9b73a986cf
The humane Coder <biswasampad.dev@gmail.com>
2024-06-04 01:15:20 +0530

    requirements.txt modified + code optimization

--- dq_module.py
+++ dq_module.py
@@ -2,257 +2,139 @@ import json
 import os
 import configparser
 import shutil
-
-from pprint import pprint
+import logging
 
 import requests
 from tqdm import tqdm
 from pymongo import MongoClient
 import pandas as pd
 
-
 class DQModule:
-
     def __init__(self) -> None:
+        self.load_config()
+        self.mongo_client = MongoClient(self.mongo_url)
+        self.db = self.mongo_client[self.mongo_client_name]
+        self.match_ids = []
+        self.match_urls = {}
+
+    def load_config(self) -> None:
         url_file_path = os.path.join('configs', 'urls.json')
+        config_file_path = os.path.join('configs', 'config.ini')
+
         with open(url_file_path, 'r', encoding='utf-8') as file:
             self.urls = json.load(file)
 
         self.config = configparser.ConfigParser()
-        self.config.read(os.path.join('configs', 'config.ini'))
-        mongo_url = self.config['MongoDB']['url']
-        client = self.config['MongoDB']['client']
-        self.client = MongoClient(mongo_url)
-        self.db = self.client[client]
-        self.match_ids = []
-        self.match_urls = {}
+        self.config.read(config_file_path)
+
+        self.mongo_url = self.config['MongoDB']['url']
+        self.mongo_client_name = self.config['MongoDB']['client']
 
     def get_data(self) -> None:
-        """this function is the master controller"""
         try:
-            match_details_url = self.urls['baseUrl'] + \
-                self.urls['onMatchLinks']
-            pprint("Updating schedules")
-            match_details = self.get_match_details(match_details_url)
-
-            if match_details:
-                pprint("schedules acquired")
-                pprint("Fetching schedules")
-                self.get_match_ids()
-
-                print("match ids acquired \n configuring new urls")
-
-                self.configure_match_urls()
-                print("urls configured \n fetching data by new url")
-                self.get_match_data_by_urls()
-                # converting to dataframe
-                print("Converting to dataframe")
-                self.convert_to_dataframe()
-
+            self.update_schedules()
+            self.get_match_ids()
+            self.configure_match_urls()
+            self.fetch_data_by_urls()
+            self.convert_to_dataframe()
         except Exception as e:
-            pprint(e)
+            logging.error(f"Error in get_data: {e}")
 
-    def get_match_details(self, match_details_url) -> any:
-        """this function gets match schedules from AWS"""
+    def update_schedules(self) -> None:
         try:
-            results = requests.get(match_details_url, timeout=1500)
-            cleaned_data_str = results.content.decode(
-                "utf-8").replace("onMatchLinks(", "").rstrip(");'")
-            match_schedules = json.loads(cleaned_data_str)
+            match_details_url = f"{self.urls['baseUrl']}{self.urls['onMatchLinks']}"
+            logging.info("Updating schedules")
+            if self.fetch_and_store_schedules(match_details_url):
+                logging.info("Schedules updated successfully")
+        except requests.RequestException as e:
+            logging.error(f"Error fetching match schedules: {e}")
 
-            # inserting schedules to mongodb
-            collection = self.db[self.config['MongoDB']['schedules']]
-            collection.drop()
-            collection.insert_many(match_schedules)
-            return True
+    def fetch_and_store_schedules(self, url: str) -> bool:
+        response = requests.get(url, timeout=1500)
+        cleaned_data_str = response.content.decode("utf-8").replace("onMatchLinks(", "").rstrip(");'")
+        match_schedules = json.loads(cleaned_data_str)
 
-        except Exception as e:
-            print("Exception occured while getting match schedules:", e)
-            return False
-
-    def get_match_ids(self) -> any:
-        """this function gets the match ids from aws"""
-        try:
-            collection = self.db["schedules"]
-            matches = collection.find()
-            match_ids = []
-            for match in matches:
-                match_ids.append(match["smId"])
-            self.match_ids = match_ids
-            return True
-
-        except Exception as e:
-            print("Exception occured while getting match history from database", e)
-
-    def configure_match_urls(self) -> bool:
-        """this function configures static urls as per the match ids"""
-        urls = self.urls
-        match_urls = {}
-        for item in self.match_ids:
-            match_urls[str(item) + 'in_1'] = urls.get('onScoring_ing1_p1') + \
-                str(item) + urls.get('onScoring_ing1_p2')
-            match_urls[str(item) + 'in_2'] = urls.get('onScoring_ing2_p1') + \
-                str(item) + urls.get('onScoring_ing2_p2')
-        self.match_urls = match_urls
-        # print(match_urls)
+        collection = self.db[self.config['MongoDB']['schedules']]
+        collection.drop()
+        collection.insert_many(match_schedules)
         return True
 
-    def get_match_data_by_urls(self) -> bool:
-        """this function pulls data from aws services """
-        base_url = self.urls.get("baseUrl")
+    def get_match_ids(self) -> None:
+        try:
+            collection = self.db["schedules"]
+            self.match_ids = [match["smId"] for match in collection.find()]
+        except Exception as e:
+            logging.error(f"Error getting match IDs: {e}")
+
+    def configure_match_urls(self) -> None:
+        self.match_urls = {}
+        for item in self.match_ids:
+            self.match_urls[f"{item}in_1"] = f"{self.urls['onScoring_ing1_p1']}{item}{self.urls['onScoring_ing1_p2']}"
+            self.match_urls[f"{item}in_2"] = f"{self.urls['onScoring_ing2_p1']}{item}{self.urls['onScoring_ing2_p2']}"
+
+    def fetch_data_by_urls(self) -> None:
         total_innings_data = []
+
         try:
             for key, value in tqdm(self.match_urls.items(), desc="Processing URLs"):
-                complete_url = base_url + value
-
-                results = requests.get(complete_url, timeout=15000)
-                cleaned_data_str = results.content.decode(
-                    "utf-8").replace("onScoring(", "").rstrip(");'")
-                # print(cleaned_data_str)
-
+                complete_url = f"{self.urls['baseUrl']}{value}"
+                response = requests.get(complete_url, timeout=15000)
+                cleaned_data_str = response.content.decode("utf-8").replace("onScoring(", "").rstrip(");'")
                 innings_data = json.loads(cleaned_data_str)
                 total_innings_data.append(innings_data)
 
             collection = self.db[self.config['MongoDB']['innings']]
             collection.drop()
             collection.insert_many(total_innings_data)
+            logging.info("Innings data fetched and stored successfully")
+        except requests.RequestException as e:
+            logging.error(f"Error fetching innings data: {e}")
 
-            pprint("innings captured")
-            return True
+    def convert_to_dataframe(self) -> None:
+        try:
+            collection = self.db[self.config['MongoDB']['innings']]
+            documents = collection.find()
+            self.save_dataframes(documents)
         except Exception as e:
-            print("Exception occurred while getting innings data:", e)
-            return False
+            logging.error(f"Error converting to DataFrame: {e}")
 
-    def convert_to_dataframe(self) -> bool:
-        """this function converts the innings data present in the mongodb to different dataframes"""
-
-        collection = self.db[self.config['MongoDB']['innings']]
-        documents = collection.find()
-
+    def save_dataframes(self, documents) -> None:
         output_folder = "outputs"
+        os.makedirs(output_folder, exist_ok=True)
 
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        else:
-            shutil.rmtree(output_folder)
-            os.makedirs(output_folder)
-        batting_card_all = []
-        extras_all = []
-        fall_of_wickets_all = []
-        wagon_wheel_all = []
-        partnership_scores_all = []
-        bowling_card_all = []
-        manhattan_graph_all = []
-        manhattan_wickets_all = []
-        over_history_all = []
-        wagon_wheel_summery_all = []
-        batting_head_to_head_all = []
-        bowling_head_to_head_all = []
+        data_dict = {
+            "batting_card": [],
+            "extras": [],
+            "fall_of_wickets": [],
+            "wagon_wheel": [],
+            "partnership_scores": [],
+            "bowling_card": [],
+            "manhattan_graph": [],
+            "manhattan_wickets": [],
+            "over_history": [],
+            "wagon_wheel_summery": [],
+            "batting_head_to_head": [],
+            "bowling_head_to_head": []
+        }
 
         for document in documents:
-            batting_card = document['Innings1']['BattingCard'] if 'Innings1' in document and 'BattingCard' in document[
-                'Innings1'] else document['Innings2']['BattingCard']
-            batting_card_df_s = pd.DataFrame(batting_card)
-            batting_card_all.append(batting_card_df_s)
+            self.extract_document_data(document, data_dict)
 
-            extras = document['Innings1']['Extras'] if 'Innings1' in document and 'Extras' in document['Innings1'] else document['Innings2']['Extras']
-            extras_df_s = pd.DataFrame(extras)
-            extras_all.append(extras_df_s)
+        for key, data in data_dict.items():
+            if data:
+                combined_df = pd.concat(data, ignore_index=True)
+                combined_df.to_csv(os.path.join(output_folder, f"{key}.csv"), index=False)
 
-            fall_of_wickets = document['Innings1']['FallOfWickets'] if 'Innings1' in document and 'FallOfWickets' in document[
-                'Innings1'] else document['Innings2']['FallOfWickets']
-            fall_of_wickets_df_s = pd.DataFrame(fall_of_wickets)
-            fall_of_wickets_all.append(fall_of_wickets_df_s)
+    def extract_document_data(self, document, data_dict) -> None:
+        for key in data_dict.keys():
+            if 'Innings1' in document and key in document['Innings1']:
+                df = pd.DataFrame(document['Innings1'][key])
+                data_dict[key].append(df)
+            elif 'Innings2' in document and key in document['Innings2']:
+                df = pd.DataFrame(document['Innings2'][key])
+                data_dict[key].append(df)
 
-            wagon_wheel = document['Innings1']['WagonWheel'] if 'Innings1' in document and 'WagonWheel' in document[
-                'Innings1'] else document['Innings2']['WagonWheel']
-            wagon_wheel_df_s = pd.DataFrame(wagon_wheel)
-            wagon_wheel_all.append(wagon_wheel_df_s)
-
-            partnership_scores = document['Innings1']['PartnershipScores'] if 'Innings1' in document and 'PartnershipScores' in document[
-                'Innings1'] else document['Innings2']['PartnershipScores']
-            partnership_scores_df_s = pd.DataFrame(partnership_scores)
-            partnership_scores_all.append(partnership_scores_df_s)
-
-            bowling_card = document['Innings1']['BowlingCard'] if 'Innings1' in document and 'BowlingCard' in document[
-                'Innings1'] else document['Innings2']['BowlingCard']
-            bowling_card_df_s = pd.DataFrame(bowling_card)
-            bowling_card_all.append(bowling_card_df_s)
-
-            manhattan_graph = document['Innings1']['ManhattanGraph'] if 'Innings1' in document and 'ManhattanGraph' in document[
-                'Innings1'] else document['Innings2']['ManhattanGraph']
-            manhattan_graph_df_s = pd.DataFrame(manhattan_graph)
-            manhattan_graph_all.append(manhattan_graph_df_s)
-
-            manhattan_wickets = document['Innings1']['ManhattanWickets'] if 'Innings1' in document and 'ManhattanWickets' in document[
-                'Innings1'] else document['Innings2']['ManhattanWickets']
-            manhattan_wickets_df_s = pd.DataFrame(manhattan_wickets)
-            manhattan_wickets_all.append(manhattan_wickets_df_s)
-
-            over_history = document['Innings1']['OverHistory'] if 'Innings1' in document and 'OverHistory' in document[
-                'Innings1'] else document['Innings2']['OverHistory']
-            over_history_df_s = pd.DataFrame(over_history)
-            over_history_all.append(over_history_df_s)
-
-            wagon_wheel_summery = document['Innings1']['WagonWheelSummary'] if 'Innings1' in document and 'WagonWheelSummary' in document[
-                'Innings1'] else document['Innings2']['WagonWheelSummary']
-            wagon_wheel_summery_df_s = pd.DataFrame(wagon_wheel_summery)
-            wagon_wheel_summery_all.append(wagon_wheel_summery_df_s)
-
-            batting_head_to_head = document['Innings1']['battingheadtohead'] if 'Innings1' in document and 'battingheadtohead' in document[
-                'Innings1'] else document['Innings2']['battingheadtohead']
-            batting_head_to_head_df_s = pd.DataFrame(batting_head_to_head)
-            batting_head_to_head_all.append(batting_head_to_head_df_s)
-
-            bowling_head_to_head = document['Innings1']['bowlingheadtohead'] if 'Innings1' in document and 'bowlingheadtohead' in document[
-                'Innings1'] else document['Innings2']['bowlingheadtohead']
-            bowling_head_to_head_df_s = pd.DataFrame(bowling_head_to_head)
-            bowling_head_to_head_all.append(bowling_head_to_head_df_s)
-
-        batting_card_df = pd.concat(batting_card_all, ignore_index=True)
-        extras_df = pd.concat(extras_all, ignore_index=True)
-        fall_of_wickets_df = pd.concat(fall_of_wickets_all, ignore_index=True)
-        wagon_wheel_df = pd.concat(wagon_wheel_all, ignore_index=True)
-        partnership_scores_df = pd.concat(
-            partnership_scores_all, ignore_index=True)
-        bowling_card_df = pd.concat(bowling_card_all, ignore_index=True)
-        manhattan_graph_df = pd.concat(manhattan_graph_all, ignore_index=True)
-        manhattan_wickets_df = pd.concat(
-            manhattan_wickets_all, ignore_index=True)
-        over_history_df = pd.concat(over_history_all, ignore_index=True)
-        wagon_wheel_summery_df = pd.concat(wagon_wheel_all, ignore_index=True)
-        batting_head_to_head_df = pd.concat(
-            batting_head_to_head_all, ignore_index=True)
-        bowling_head_to_head_df = pd.concat(
-            bowling_head_to_head_all, ignore_index=True)
-
-        batting_card_df.to_csv(os.path.join(
-            output_folder, "batting_card.csv"), index=False)
-        extras_df.to_csv(os.path.join(
-            output_folder, "extras.csv"), index=False)
-        fall_of_wickets_df.to_csv(os.path.join(
-            output_folder, "fall_of_wickets.csv"), index=False)
-        wagon_wheel_df.to_csv(os.path.join(
-            output_folder, "wagon_wheel.csv"), index=False)
-        partnership_scores_df.to_csv(os.path.join(
-            output_folder, "partnership_scores.csv"), index=False)
-        bowling_card_df.to_csv(os.path.join(
-            output_folder, "bowling_card.csv"), index=False)
-        manhattan_graph_df.to_csv(os.path.join(
-            output_folder, "manhattan_graph.csv"), index=False)
-        manhattan_wickets_df.to_csv(os.path.join(
-            output_folder, "manhattan_wickets.csv"), index=False)
-        over_history_df.to_csv(os.path.join(
-            output_folder, "over_history.csv"), index=False)
-        wagon_wheel_summery_df.to_csv(os.path.join(
-            output_folder, "wagon_wheel_summery.csv"), index=False)
-        batting_head_to_head_df.to_csv(os.path.join(
-            output_folder, "batting_head_to_head.csv"), index=False)
-        bowling_head_to_head_df.to_csv(os.path.join(
-            output_folder, "bowling_head_to_head.csv"), index=False)
-
-        return True
-
-
-stats_class = DQModule()
-stats_class.get_data()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    dq_module = DQModule()
+    dq_module.get_data()
